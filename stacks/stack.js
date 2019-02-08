0d4666e465e370162b6d7d4222fdda4f2c288b8b
utkarsh kandpal <utkarshkpal@gmail.com>
2019-02-08 19:46:19 +0530

    postfix evaluation

--- stacks/stack.js
+++ stacks/stack.js
@@ -115,5 +115,100 @@ function checkPalindrome(word) {
     }
 }
 
-checkPalindrome('nitin');
-// checkPalindrome('ksajdksj'); checkPalindrome('Nitin');
+// checkPalindrome('nitin'); checkPalindrome('ksajdksj');
+// checkPalindrome('Nitin');
+
+/*
+Check if paranthesis is balanced
+1)push first elem onto stack;
+2)check if next element is matching with the top of the stack.
+   If yes then : pop the element
+   If not then : push the element also
+3) repeat 2) until the length of string
+4)if the stack is not empty then return false else return true
+*/
+
+function parenthesisBalanced(string) {
+
+    let s = new Stack();
+    const n = string.length;
+
+    for (let i = 0; i < n; i++) {
+        if (i == 0) {
+            s.push(string[i]);
+        } else {
+            if (checkIfMatchingBracket(s.peek(), string[i])) {
+                s.pop();
+            } else {
+                s.push(string[i]);
+            }
+        }
+    }
+
+    if (s.length() == 0) {
+        return true;
+    } else {
+
+        return false;
+    }
+
+    function checkIfMatchingBracket(bracket1, bracket2) {
+
+        const matchingBrackets = {
+            '[': ']',
+            '{': '}',
+            '(': ')'
+        }
+
+        if (matchingBrackets[bracket1] === bracket2) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+};
+
+// console.log(parenthesisBalanced('{[()]}'));
+// console.log(parenthesisBalanced('{[(])}'));
+// console.log(parenthesisBalanced('{{[[(())]]}}'));
+
+/*
+Evaluation of postfix expression
+1) push first 2 elements onto the stack
+2) check if element is number or operator.
+   if operator : 1) b = s.pop();
+                 2) a = s.pop();
+                 3)  s.push(a operator b);
+   if number push element onto stack
+3) Repeat 2) until for whole string;
+
+*/
+
+function postfixEvaluation(string) {
+    const n = string.length;
+    const s = new Stack();
+
+    for (let i = 0; i < n; i++) {
+
+        if (i == 0 || i == 1) {
+            s.push(string[i]);
+        } else {
+            if (Number.isNaN(Number(string[i]))) {
+
+                const operator = string[i];
+                const b = s.pop();
+                const a = s.pop();
+                const result = eval(String(a).concat(operator, b));
+                s.push(result);
+            } else {
+                s.push(string[i]);
+            }
+        }
+
+    }
+
+    return s.peek();
+}
+
+console.log(postfixEvaluation('231*+9-'));
