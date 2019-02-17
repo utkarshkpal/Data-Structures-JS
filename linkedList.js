684e9415af0a33b2eaf0c2b5ef87f1f222a02e8e
utkarsh kandpal <utkarshkpal@gmail.com>
2019-02-17 19:26:39 +0530

    ll application

--- linkedList.js
+++ linkedList.js
@@ -19,6 +19,7 @@ linkedList.prototype.insert = function (data) {
 }
 
 linkedList.prototype.print = function () {
+
     let currNode = this.head;
     let printString = 'head';
 
@@ -123,11 +124,9 @@ function RemoveDuplicatesSorted(head) {
 
 }
 
-const ll1 = new linkedList();
-
-// ll1.insert(12); ll1.insert(12); ll1.insert(12); ll1.insert(11);
-// ll1.insert(12); ll1.insert(21); ll1.insert(41); ll1.insert(43);
-// ll1.insert(21); ll1.insert(21); printList(ll1.head);
+// const ll1 = new linkedList(); ll1.insert(12); ll1.insert(12); ll1.insert(12);
+// ll1.insert(11); ll1.insert(12); ll1.insert(21); ll1.insert(41);
+// ll1.insert(43); ll1.insert(21); ll1.insert(21); printList(ll1.head);
 // RemoveDuplicates(ll1.head); printList(ll1.head); ll1.insert(1);
 // ll1.insert(1); ll1.insert(2); ll1.insert(3); ll1.insert(3); ll1.insert(4);
 // ll1.insert(4); ll1.insert(4); printList(ll1.head);
@@ -162,12 +161,232 @@ function deleteMiddleElement(head) {
 
 }
 
+/*
 ll1.insert(1);
 ll1.insert(2);
 ll1.insert(3);
 ll1.insert(4);
 ll1.insert(5);
-
 printList(ll1.head);
 deleteMiddleElement(ll1.head);
 printList(ll1.head);
+*/
+
+/*
+ Sum of 2 linked list
+
+*/
+
+function generateNumberfromLL(head) {
+
+    let currNode = head.next;
+    let multiplier = 1;
+    let number = 0;
+    while (currNode != null) {
+
+        number = multiplier * currNode.data + number;
+        multiplier *= 10;
+        currNode = currNode.next;
+    }
+
+    return number;
+}
+
+function generateLLfromNumber(number) {
+
+    function insertAtFront(head, number) {
+        const newNode = new Node(number);
+        newNode.next = head.next;
+        head.next = newNode;
+    }
+
+    const ll3 = new linkedList();
+
+    while (number / 10 !== 0) {
+        const digit = number % 10;
+        insertAtFront(ll3.head, digit);
+        number = Math.floor(number / 10);
+    }
+
+    return ll3;
+
+}
+
+function addTwoLL(head1, head2) {
+    const first = generateNumberfromLL(head1);
+    const second = generateNumberfromLL(head2);
+    const sum = first + second;
+    return generateLLfromNumber(sum);
+
+}
+
+/*
+const ll1 = new linkedList();
+
+ll1.insert(7);
+ll1.insert(2);
+
+const ll2 = new linkedList();
+
+ll2.insert(5);
+ll2.insert(2);
+ll2.insert(1);
+
+//Using ll print function and calling it the newly returned ll.
+linkedList
+    .prototype
+    .print
+    .call(sumOfLL(ll1.head, ll2.head));
+*/
+
+// linkedList     .prototype     .print     .call(generateLLfromNumber(1013));
+
+/*
+LL stored in reverse order
+WAY 2 : in single pass
+7->1->6  617
+9->2  29
+
+  7+9 = 16 . [6] in  the list. 1 carry;
+  2+1 +1(c) = 4 .[4] in the list . 0 carry.
+  6+ 0 +0(c) = 6. [6] in list. carry 0 .
+*/
+
+function sumOfLL(head1, head2) {
+    let currNode1 = head1.next;
+    let currNode2 = head2.next;
+    const ll3 = new linkedList();
+    let carry = 0;
+
+    function insertAtFront(head, number) {
+        const newNode = new Node(number);
+        newNode.next = head.next;
+        head.next = newNode;
+    }
+
+    while (currNode1 != null || currNode2 != null) {
+        let firstDigit = 0,
+            secondDigit = 0;
+        if (currNode1 != null) {
+
+            firstDigit = currNode1.data;
+        }
+        if (currNode2 != null) {
+
+            secondDigit = currNode2.data;
+        }
+
+        const sum = firstDigit + secondDigit + carry;
+        carry = Math.floor(sum / 10);
+        insertAtFront(ll3.head, sum % 10);
+        if (currNode1 != null) {
+            currNode1 = currNode1.next;
+        }
+        if (currNode2 != null) {
+            currNode2 = currNode2.next;
+        }
+
+    }
+    //insert last carry into LL
+    if (carry) {
+        insertAtFront(ll3.head, carry);
+    }
+
+    return ll3;
+}
+
+/*
+Check if ll is palinderome.
+
+Algo1 : 1)reverse the ll.
+        2)initialize 2 counter one with original and other with reversed.
+        3)loop through the both ll
+             if data not equal return false;
+         return true;
+
+Algo2 : 1)Push half elements onto stack.
+        2)iterate over the rest half of ll
+        compare it with the popped value from the stack.
+        3)if equal return false.
+        else return true;
+                                odd : after half iteration increment s
+        R-A-D-A-R
+            s
+                f                while(f.next!=null || f.next.next!=null)
+        S:R,A
+
+
+ */
+
+function checkLLPalindrome(head) {
+    const s = new Stack();
+    let slow = head.next;
+    let fast = head.next;
+
+    while (fast !== null && fast.next != null) {
+        // debugger;
+        s.push(slow.data);
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    //ll is odd
+    if (fast != null) {
+        slow = slow.next;
+    }
+
+    while (slow.next != null) {
+        if (slow.data != s.pop()) {
+            return false;
+        }
+        slow = slow.next;
+    }
+    return true;
+}
+
+function Stack() {
+    this.dataStore = [];
+    this.top = 0;
+
+}
+
+//push new element into the arr increment top
+Stack.prototype.push = function (elem) {
+    this.dataStore[this.top++] = elem;
+
+}
+
+Stack.prototype.printStack = function () {
+    // if (this.top == 0) {     console.log('Stack is empty'); } else {     for (let
+    // i = 0; i < this.top; i++) {         console.log('Stack', this.dataStore[i]);
+    // } }
+    console.log(this.dataStore);
+
+}
+
+// return the elem at top index, , delete the element
+Stack.prototype.pop = function (elem) {
+    return this.dataStore[--this.top];
+
+}
+
+Stack.prototype.peek = function () {
+    return this.dataStore[this.top - 1];
+}
+
+Stack.prototype.clear = function () {
+    this.top = 0;
+}
+
+Stack.prototype.length = function () {
+    return this.top;
+}
+
+const ll1 = new linkedList();
+
+ll1.insert(1);
+ll1.insert(2);
+ll1.insert(3);
+ll1.insert(2);
+ll1.insert(1);
+
+console.log(checkLLPalindrome(ll1.head));
